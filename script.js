84be42cd4a0f4fd5beda156225d44c4a62cfee4d
pongsathon346 <pongsathon_j@cmu.ac.th>
2021-05-28 17:09:13 +0700

    3-show log

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-var newText='WavE'
-document.getElementById('no').innerHTML=newText
+document.getElementById('ok').addEventListener('click',function(e){
+    alert('ok is clicked')
+    console.log(e)
+}
+)
+document.getElementById('cancel').addEventListener('mousemove',function(e){  
+    console.log('x = '+e.clientX)
+    console.log('y = '+e.clientY)
+}
+)
